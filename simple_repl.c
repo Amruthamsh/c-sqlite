9699ad339b3e4c5e1de9d16ba601d9cc2b0491f3
Amruthamsh A <amruthaamsh@gmail.com>
2025-12-23 16:11:35 +0530

    meta commands and sql compiler

--- simple_repl.c
+++ simple_repl.c
@@ -3,7 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Making a Simple REPL
+typedef enum {
+    META_SUCCESS,
+    META_UNKNOWN_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNKNOWN_COMMAND
+} PrepareCommandResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct 
+{
+    StatementType type;
+} Statement;
 
 typedef struct 
 {
@@ -18,6 +36,47 @@ InputBuffer* create_new_buffer(){
     input_buffer->buffer_length=0;
     input_buffer->input_length=0;
     return input_buffer;
+}
+
+void clear_buffer(InputBuffer* input_buffer){
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(strcmp(input_buffer->buffer, ".exit") == 0){
+        clear_buffer(input_buffer);
+        printf("Quitting...\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_UNKNOWN_COMMAND;
+    }
+}
+
+PrepareCommandResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    if(strncmp(input_buffer->buffer, "insert", 6) == 0){
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if(strcmp(input_buffer->buffer, "select") == 0){
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNKNOWN_COMMAND;
+}
+
+void execute_statement(Statement* statement){
+    switch (statement->type)
+    {
+    case STATEMENT_SELECT:
+        printf("Select statement has successfully run\n");
+        break;
+    case STATEMENT_INSERT:
+        printf("Insert statement has successfully run\n");
+        break;
+    }
 }
 
 void cl_default_print(){
@@ -36,22 +95,36 @@ void read_input(InputBuffer* input_buffer){
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void clear_buffer(InputBuffer* input_buffer){
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
-
 int main(){
     InputBuffer* input_buffer = create_new_buffer();
     while(true){
         cl_default_print();
         read_input(input_buffer);
 
-        if(strcmp(input_buffer->buffer, ".exit") == 0){
-            clear_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+        if(input_buffer->buffer[0] == '.'){
+            switch (do_meta_command(input_buffer))
+            {
+                case META_SUCCESS:
+                    continue;
+                case META_UNKNOWN_COMMAND:
+                    printf("Unrecognized comman %s", input_buffer->buffer);
+                    continue;
+            }
         }
 
-        printf("Command not found: `%s`\n", input_buffer->buffer);
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
+        {
+            case PREPARE_SUCCESS:
+                break;
+            
+            case PREPARE_UNKNOWN_COMMAND:
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
+
     }
 }
