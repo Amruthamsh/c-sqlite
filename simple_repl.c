da63ce6a42c5023dbdc4256e1e2ab4f3ef223788
Amruthamsh A <amruthaamsh@gmail.com>
2025-12-24 14:43:52 +0530

    bug fixes

--- simple_repl.c
+++ simple_repl.c
@@ -17,7 +17,9 @@ typedef enum {
 typedef enum {
   PREPARE_SUCCESS,
   PREPARE_UNKNOWN_COMMAND,
-  PREPARE_SYNTAX_ERROR
+  PREPARE_SYNTAX_ERROR,
+  PREPARE_STRING_TOO_LONG,
+  PREPARE_INVALID_ID_TYPE
 } PrepareCommandResult;
 
 typedef enum {
@@ -32,8 +34,8 @@ typedef enum {
 
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -109,15 +111,57 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
   }
 }
 
+PrepareCommandResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if(id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+
+    if(id < 0){
+        return PREPARE_INVALID_ID_TYPE;
+    }
+
+    if(strcmp(id_string, "0") != 0 && id == 0){ // user entered a character instead of a number
+        // srand(time(NULL));
+        // id = rand() % 100;
+        return PREPARE_INVALID_ID_TYPE;
+    }
+
+    if(strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    if(strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 PrepareCommandResult prepare_command(InputBuffer* input_buffer, Statement* statement) {
   if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+    // statement->type = STATEMENT_INSERT;
+    // int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
 
-    if(args_assigned < 3) {
-      return PREPARE_SYNTAX_ERROR;
-    }
-    return PREPARE_SUCCESS;
+    // if(args_assigned < 3) {
+    //   return PREPARE_SYNTAX_ERROR;
+    // }
+    // return PREPARE_SUCCESS;
+
+    // call the function prepare_insert
+    return prepare_insert(input_buffer, statement);
   }
 
   if(strcmp(input_buffer->buffer, "select") == 0) {
@@ -233,6 +277,14 @@ int main(int argc, char* argv[]) {
     {
     case PREPARE_SUCCESS:
       break;
+
+    case PREPARE_STRING_TOO_LONG:
+        printf("Insertion failed: Provided string exceeds the maximum supported length\n");
+        continue;
+
+    case PREPARE_INVALID_ID_TYPE:
+        printf("ID must be a valid positive integer\n");
+        continue;
 
     case (PREPARE_SYNTAX_ERROR):
       printf("Syntax error. Could not parse statement.\n");
